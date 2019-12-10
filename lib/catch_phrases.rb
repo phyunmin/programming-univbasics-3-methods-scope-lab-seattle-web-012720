134a15ae09bb57aad843a2e3f58564f577cf9706
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-10 23:37:29 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,20 @@
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  string_var = "It's Dangerous To Go Alone! Take This."
+  puts string_var
+end
+
+def any_phrase(phrase)
+  puts phrase
+  return phrase
 end
