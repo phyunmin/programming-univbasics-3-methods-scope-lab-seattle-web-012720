8a3733295ff4eef76544fb50b9987dd27ebcdf00
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-10 23:33:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
-$status
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
   phrase = "It's-a me, Mario!"
   puts phrase
 end
