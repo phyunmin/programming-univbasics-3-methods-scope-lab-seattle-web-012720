46d18019097978aceaff61485b3bda05a9cebe23
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-10 23:30:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,16 @@
+$status
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
   puts status
+end
+
+def link
+  string_var = "It's Dangerous To Go Alone! Take This."
+  return string_var
 end
